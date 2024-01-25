8bc1c9b37c3cdd831879596df7b161527d3fda54
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-01-25 17:08:54 +0100

    StrengthsComponent added and modified

---
+++ src/app/strengths/strengths.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StrengthsComponent } from './strengths.component';
+
+describe('StrengthsComponent', () => {
+  let component: StrengthsComponent;
+  let fixture: ComponentFixture<StrengthsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [StrengthsComponent]
+    });
+    fixture = TestBed.createComponent(StrengthsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
