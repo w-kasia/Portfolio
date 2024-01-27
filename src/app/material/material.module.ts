c8433f2cf657b01fe7fcbb0b0f21b3d49a9ec7ed
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-01-27 11:53:10 +0100

    Toolbar added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
-const material = [MatFormFieldModule, MatInputModule, MatButtonModule];
+const material = [MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule];
 
 @NgModule({
   imports: [material],
