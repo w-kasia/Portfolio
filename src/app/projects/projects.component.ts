4eb6531d4a92287cab4bf8f913a701f12ab94946
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-01-27 21:35:43 +0100

    styles modified

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+
+
 
 }
