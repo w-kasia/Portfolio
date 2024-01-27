cebab53fa5ceae7fd7303445f7bef9d054399f18
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-01-27 21:35:43 +0100

    styles modified

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -7,14 +7,4 @@ import { Component } from '@angular/core';
 })
 export class NavigationComponent {
 
-  // @HostListener('window:scroll', ['$event'])
-
-  // onWindowScroll() {
-  //   let element = document.querySelector('.menuItemsContainer') as HTMLElement;
-  //   if (window.pageYOffset > element.clientHeight) {
-  //     element.classList.add('menuItemsContainer-inverse');
-  //   } else {
-  //     element.classList.remove('menuItemsContainer-inverse');
-  //   }
-  // }
 }
