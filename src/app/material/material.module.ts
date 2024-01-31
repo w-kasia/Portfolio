138f7362f562bd212d487eb35a2030e06fee02bc
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-01-31 22:04:40 +0100

    Hamburger Menu for small devices added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from "@angular/core";
+import {NgModule} from "@angular/core";
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
-const material = [MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule, MatDialogModule];
+const material = [MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule, MatDialogModule, MatIconModule, MatMenuModule];
 
 @NgModule({
   imports: [material],
