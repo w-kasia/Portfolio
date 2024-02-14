520c55816841a6b7cd2166b9dbb6156d2cd27fe4
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-02-14 14:04:05 +0100

    Intersection Observer added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,10 +6,34 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
   constructor(private router: Router) {}
 
   goToContact() {
     this.router.navigate(['/contact'])
+  }
+
+  ngOnInit() {
+    const buttons = document.querySelectorAll('button');
+
+     const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5,
+    }
+
+     const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fadeInLeft');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    buttons.forEach(button => {
+      observer.observe(button);
+    });
   }
 }
