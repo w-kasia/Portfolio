f9ce5529a9d30356617a07be0584e6909af94444
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-02-14 14:18:15 +0100

    Intersection Observer added

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -17,4 +17,35 @@ export class AboutComponent {
   goToContactForm() {
     this.router.navigate(['/contact']);
   }
+
+  ngOnInit() {
+    const images = document.querySelectorAll('img');
+    const paragraphs = document.querySelectorAll('p');
+    const buttons = document.querySelectorAll('button');
+
+     const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5,
+    }
+
+     const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fadeInTop');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    images.forEach(image => {
+      observer.observe(image);
+    });
+    paragraphs.forEach(paragraph => {
+      observer.observe(paragraph);
+    });
+    buttons.forEach(button => {
+      observer.observe(button);
+    });
+  }
 }
