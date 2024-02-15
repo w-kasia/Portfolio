202671845905378a99caee4fc8a346a1da25d280
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-02-15 21:00:48 +0100

    styles modified

--- src/app/strengths/strengths.component.ts
+++ src/app/strengths/strengths.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-strengths',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./strengths.component.css']
 })
 
-export class StrengthsComponent {}
+export class StrengthsComponent implements OnInit {
+  ngOnInit() {
+    const icons = document.querySelectorAll('.icon');
+
+     const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5,
+    }
+
+     const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fadeInTop');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    icons.forEach(icon => {
+      observer.observe(icon);
+    });
+  }
+}
 
